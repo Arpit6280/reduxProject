d7e4a0ba20d5ee6309c2e376228264e72be839ad
Arpit Singh Yadav <arpitsinghyadav19@gmail.com>
2023-12-01 19:20:01 +0530

    Doing Network calls and andling response the right way

--- src/App.js
+++ src/App.js
@@ -1,16 +1,66 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import Notification from './components/UI/Notification';
+import { uiAction } from './store/cartReducer';
+
+let isInitial=true;
 
 function App() {
   const isCartShown= useSelector(state=>state.ui.isCartShown)
+  const cart=useSelector(state=>state.cart)
+  const notification=useSelector(state=>state.ui.notification)
+  const dispatch=useDispatch();
+
+  useEffect(()=>{
+    const sendCartData=async()=>{
+    dispatch(uiAction.showNotification({
+      status:'pending',
+      title:'Sending..',
+      message:'Sending cart data'
+    }))
+  const response= await fetch('https://react-http-62209-default-rtdb.firebaseio.com/cart.json',{
+      method:'PUT',
+      body:JSON.stringify(cart)
+    })
+      if(!response.ok){
+        
+       
+        throw new Error('Sending cart data failed')
+      }
+    
+
+    dispatch(uiAction.showNotification({
+      status:'success',
+      title:'Success',
+      message:'Sent cart data succesfully'
+    }))
+  }
+
+  if(isInitial){
+    isInitial=false
+    return;
+  }
+  sendCartData().catch(error=>{
+    dispatch(uiAction.showNotification({
+      status:'error',
+      title:'Error',
+      message:'Sending cart data failed'
+    }))
+  })
+  },[cart,dispatch])
   console.log(isCartShown);
+  console.log(notification);
   return (
+    <>
+   {notification &&  <Notification status={notification.status} title={notification.title} message={notification.message} />}
     <Layout>
      {isCartShown && <Cart /> }
       <Products />
     </Layout>
+    </>
   );
 }
 
--- src/store/cartReducer.js
+++ src/store/cartReducer.js
@@ -2,7 +2,7 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 // createSlice
 
-const initialUIState = { isCartShown: false };
+const initialUIState = { isCartShown: false, notification: null };
 
 const uiSlice = createSlice({
   name: "ui",
@@ -11,10 +11,20 @@ const uiSlice = createSlice({
     openCloseCart(state) {
       state.isCartShown = !state.isCartShown;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+    closeNotification(state) {
+      state.notification = false;
+    },
   },
 });
 
-const cartSlice=createSlice({
+const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
@@ -25,7 +35,9 @@ const cartSlice=createSlice({
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-      state.totalQuantity++
+      console.log(existingItem);
+      // console.log(existingItem.name, existingItem.quantity);
+      state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -35,28 +47,29 @@ const cartSlice=createSlice({
           name: newItem.title,
         });
       } else {
-        existingItem.quantity=existingItem.quantity+1
-        existingItem.totalPrice=existingItem.totalPrice+newItem.price
+        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+      console.log(existingItem);
+    },
+    removeItemFromCart(state, action) {
+      state.totalQuantity--;
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
-    removeItemFromCart(state,action){
-        state.totalQuantity--;
-        const id=action.payload;
-        const existingItem=state.items.find(item=> item.id===id);
-        if(existingItem.quantity===1){
-            state.items=state.items.filter(item=>item.id!==id)
-        }else{
-            existingItem.quantity--;
-            existingItem.totalPrice=existingItem.totalPrice-existingItem.price
-        }
-    }
   },
 });
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer ,cart:cartSlice.reducer},
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 export const uiAction = uiSlice.actions;
-export const cartActions=cartSlice.actions
+export const cartActions = cartSlice.actions;
 
 export default store;
